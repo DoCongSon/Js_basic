c74afd7889f95762f8f8e5b7f3d6ab8a4da08a4d
Đỗ Công Sơn <sonanhson99@gmail.com>
2021-08-21 17:27:13 +0700

    update main.js in lesson7

--- lesson_7(function)/main.js
+++ lesson_7(function)/main.js
@@ -1,14 +1,19 @@
 /**
  * * function
- * 1. định nghĩa hàm
- * 2. truyền tham số
- * 3. arguments
+ * ?1. định nghĩa hàm
+ *  - Decalration function
+ *  - Expression function
+ *  - Arrow fucntion
+ * ?2. truyền tham số
+ * ?3. arguments
  * 
  */
 
 
 // dịnh nghĩa hàm kiểu
 // a, arr dược gọi là tham số
+// Declaration function có thể gọi trước khi định nghĩa functioon
+console.log(checkExistIdol(['a', 'b', 'x'], 'x'));
 
 function checkExistIdol(arr, a) {
     for (let index = 0; index < arr.length; index++) {
@@ -17,6 +22,23 @@ function checkExistIdol(arr, a) {
         }
     }
     return false
+}
+console.log(typeof checkExistIdol);
+// Expression
+// Expression có thể dặt tên cho function hoặc không
+
+var showMessage = function(message) {
+    console.log(message)
+}
+showMessage("Đỗ Công Sơn")
+
+setTimeout(function autoLogin() {
+    
+})
+var myObject = {
+    myFunction: function () {
+        
+    }
 }
 
 // arguments
