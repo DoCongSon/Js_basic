2b8d932511f9c5993c20656e5c270059cec5a1c6
Đỗ Công Sơn <sonanhson99@gmail.com>
2021-08-23 17:07:29 +0700

    update lesson 10 loop

--- lesson_10(loop)/main.js
+++ lesson_10(loop)/main.js
@@ -5,6 +5,7 @@
  * 3.for/of - lặp qua các giá trị(value) của một đối tượng có thể lặp lại
  * 4.while - lặp qua một khối mã trong khi một điều kiện được chỉ định là đúng
  * 5.do/while - cũng lặp lại qua một khối mã trong khi một điều kiện được chỉ định là đúng
+ * 6.break & continue
  */
 
 // * 1. for
@@ -34,10 +35,65 @@ const person = {fname:"Đỗ", lname:"Công Sơn", age:20};
 for (const key in person) {
     console.log(`${key}: ${person[key]}`);
 }
-// Câu lệnh for-in cũng có thể lặp lại các thuộc tính của Mảng
+// Câu lệnh for-in cũng có thể lặp lại các thuộc tính của Mảng với x được tự động từ 0 -> hết
 const numbers = [45, 4, 9, 16, 25];
 let txt = "";
 for (let x in numbers) {
   txt += numbers[x];
+  console.log(`${x}: ${numbers[x]}`);
 }
 console.log(txt);
+
+// * 3. for- of
+// Câu lệnh for-of lặp qua các giá trị của một đối tượng có thể lặp lại.
+// Nó cho phép bạn lặp qua các cấu trúc dữ liệu có thể lặp lại như Arrays, Strings, Maps, NodeLists,...
+// * biến - Đối với mỗi lần lặp, giá trị của thuộc tính tiếp theo được gán cho biến.
+// * Biến có thể được khai báo với const, let hoặc var.
+// * có thể lặp lại - Một đối tượng có các thuộc tính có thể lặp lại.
+
+// vd lặp for-of với mảng
+const cars = ["BMW", "Volvo", "Mini"];
+for (const car of cars) {
+    console.log(car);
+}
+
+// vd lặp for-of voies array
+let language = "JavaScript";
+for (const x of language) {
+    console.log(x);
+}
+
+// * 4. while
+// lăp khi điều kiện trong while đúng
+// chú ý diều kiện nếu ko sẽ thành vòng lặp vô hạn
+
+// ! chú ý i = 5 từ vd trên
+while (i < 10) {
+    console.log("The number is " + i);
+    i++;
+}
+// * 5. do-while 
+// giống while nhưng luôn chạy lần đầu tiên xong mới kiểm tra điều kiện
+// ! chú ý i = 10 từ vd trên
+do {
+    console.log("Number is " + i);
+    i++;
+}
+while (i < 10);
+
+// * 6. break & continue
+// break dùng để thoát khỏi vòng lặp
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+    if (i == 5) {
+        break;
+    }
+}
+// continue dùng để nhảy tới bước lặp tiếp theo bỏ qua câu lệnh sau nó
+console.log('------------------------------------');
+for (let i = 0; i < 10; i++) {
+    if (i % 2 != 0) {
+        continue
+    }
+    console.log(i);
+}
