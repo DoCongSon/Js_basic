bde16239b0da5039dd20831ba11ca701ea735750
Đỗ Công Sơn <sonanhson99@gmail.com>
2021-09-03 23:05:42 +0700

    update

--- lesson_13(DOM-2)/main.js
+++ lesson_13(DOM-2)/main.js
@@ -1,4 +1,4 @@
-// DOM CSS
+// ! DOM CSS (style property)
 // cách 1 dùng để set từng thuộc tính
 document.getElementById('box').style.backgroundColor = 'orange';
 document.getElementById('box').innerText = 'Hello'
@@ -7,7 +7,21 @@ Object.assign(document.getElementById('box').style, {
    height: '150px',
    width: '200px',
    fontSize: '50px',
-   color: 'white',
+   // color: 'white',
    textAlign: 'center',
    lineHeight: '150px'
 })
+// ! classList property
+// dùng để thêm, sửa, xoá, làm việc với class trong html qua js
+// 1. add dùng để thêm class
+document.getElementById('box').classList.add('red-text')
+// 2. contains đùng để kiểm tra xem class có tồn tại trong element đó không trả về true fales
+console.log(document.getElementById('box').classList.contains('red-text'));
+console.log(document.getElementById('box').classList.contains('yellow-text'));
+// 3. remove dùng để xoá class
+document.getElementById('box').classList.remove('red-text')
+// 4. toggel dùng để thêm class nếu element chưa có class đó và xoá class đó đi nếu trong element đã có
+document.getElementById('box').classList.add('yellow-text')
+setInterval(function (params) {
+   document.getElementById('box').classList.toggle('green-text')
+}, 300)
